5734b6f74552d8e4db70e94a7f7106ee125c08a2
Manuel Meneses <hbiaser132@gmail.com>
2025-01-25 22:51:57 -0400

    Agregar máscara a las capturas de pantalla en las pruebas de deslizamiento de Tinder

--- e2e/01-tinder-swipe/index.spec.ts
+++ e2e/01-tinder-swipe/index.spec.ts
@@ -24,7 +24,9 @@ test("01-tinder-swipe", async ({ page }) => {
     for (let i = 0; i < 400; i++) {
       await page.mouse.move(centroX + i, centroY);
       if (i % 15 === 0) {
-        await expect(page).toHaveScreenshot(`swipe-right-${i / 15}.png`);
+        await expect(page).toHaveScreenshot(`swipe-right-${i / 15}.png`, {
+          mask: [page.locator("h2")],
+        });
       }
     }
     await page.mouse.up();
@@ -52,7 +54,9 @@ test("01-tinder-swipe", async ({ page }) => {
     for (let i = 0; i < 400; i++) {
       await page.mouse.move(centroX - i, centroY);
       if (i % 15 === 0) {
-        await expect(page).toHaveScreenshot(`swipe-left-${i / 15}.png`);
+        await expect(page).toHaveScreenshot(`swipe-left-${i / 15}.png`, {
+          mask: [page.locator("h2")],
+        });
       }
     }
     await page.mouse.up();
